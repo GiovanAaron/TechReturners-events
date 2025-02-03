fda2e720c45caac7bc60912c6b901d8297af7ab2
Javan <Javan@Javans-MacBook-Pro.local>
2025-02-03 14:01:27 +0000

    removes secret key exposing console log and changes config to include dev

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -26,7 +26,7 @@ interface Config {
 }
 const config: Config = {};
 
-if (ENV === 'production') {
+if (ENV === 'production' || ENV === 'development') {
   config.connectionString = process.env.PGDATABASE;
   config.max = 2,
   config.user = process.env.DB_USER || 'postgres', // Supabase database user
